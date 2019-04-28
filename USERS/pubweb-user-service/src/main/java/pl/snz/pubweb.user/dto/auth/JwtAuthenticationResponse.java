80364641feddd9c605e10ac96405e0d998936e3c
lukgrebo <lukgrebo@gmail.com>
2019-04-28 22:17:11 +0200

    Added user id to login response

--- USERS/pubweb-user-service/src/main/java/pl/snz/pubweb.user/dto/auth/JwtAuthenticationResponse.java
+++ USERS/pubweb-user-service/src/main/java/pl/snz/pubweb.user/dto/auth/JwtAuthenticationResponse.java
@@ -1,13 +1,13 @@
 package pl.snz.pubweb.user.dto.auth;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class JwtAuthenticationResponse {
     private String accessToken;
-    private String tokenType = "Bearer";
+    private final String tokenType = "Bearer";
+    private Long userId;
 
-    public JwtAuthenticationResponse(String token) {
-        this.accessToken = token;
-    }
 }

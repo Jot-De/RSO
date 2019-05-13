b109180a95a381e3f3d6174bf076c341e18ced69
Paweł Walkowiak <pawel.walkowiak@outlook.com>
2019-05-13 22:59:00 +0200

    Docker compose for project

--- Services/runPubService.bat
+++ Services/runPubService.bat
@@ -1,3 +1,3 @@
 call mvn clean install -f pub-service/pom.xml -DskipTests
-call java -jar pub-service/target/pub-service-1.0.0.jar
+call java -Dspring.profiles.active=docker -jar pub-service/target/pub-service-1.0.0.jar
 cmd /k

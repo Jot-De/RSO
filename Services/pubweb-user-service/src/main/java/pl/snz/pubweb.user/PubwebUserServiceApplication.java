8096436bcd6caa85b5896490d8c34330ebd2bda0
lukgrebo <lukgrebo@gmail.com>
2019-05-15 23:13:58 +0200

    Fixed component scan in user service. Error responses will be more meaningfull

--- Services/pubweb-user-service/src/main/java/pl/snz/pubweb.user/PubwebUserServiceApplication.java
+++ Services/pubweb-user-service/src/main/java/pl/snz/pubweb.user/PubwebUserServiceApplication.java
@@ -3,7 +3,9 @@ package pl.snz.pubweb.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("pl.snz.pubweb")
 @SpringBootApplication
 @EnableDiscoveryClient
 public class PubwebUserServiceApplication {

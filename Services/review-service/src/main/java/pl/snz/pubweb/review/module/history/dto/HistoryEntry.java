acff242c30b9edbc9b5b5a559aa6e17a9a033108
lukgrebo <lukgrebo@gmail.com>
2019-05-15 20:09:32 +0200

    Added review service configurations

--- Services/review-service/src/main/java/pl/snz/pubweb/review/module/history/dto/HistoryEntry.java
+++ Services/review-service/src/main/java/pl/snz/pubweb/review/module/history/dto/HistoryEntry.java
@@ -2,8 +2,11 @@ package pl.snz.pubweb.review.module.history.dto;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class HistoryEntry extends HistoryEntryBrief {
     private String oldText;
     private int oldStars;
+    private LocalDateTime check = LocalDateTime.now();
 }
